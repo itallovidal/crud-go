8bb27cc0f62fec8843edde3fccfdb340f8e2ba87
Itallo Vidal <itallovidalll@gmail.com>
2025-02-07 00:44:07 -0300

    create new user list

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,7 +1,46 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"log/slog"
+	"net/http"
+	"rocketseat-desafio-1/interfaces"
 
-func UserController(w http.ResponseWriter, r *http.Request )   {
-	w.Write([]byte("Oi!"))
+	"github.com/google/uuid"
+)
+
+
+func CreateUserController(db map[string]interfaces.User) http.HandlerFunc{
+	return func (w http.ResponseWriter, r *http.Request ) {
+		body, bodyError := io.ReadAll(r.Body)
+
+		if bodyError != nil {
+			slog.Error("Body parse error.")
+			w.Write([]byte("Erro interno de servidor."))
+		}
+
+		var userList []interfaces.User
+
+		jsonError := json.Unmarshal(body, &userList)
+
+		if jsonError != nil {
+			slog.Error("Json parse error.")
+			w.Write([]byte("Erro interno de servidor."))
+		}
+
+		for _, newUser := range userList {
+			id := uuid.NewString()
+			db[id] = newUser
+		}
+
+		response := interfaces.GenericResponse{
+			Message: "Usuários cadastrados com sucesso!",
+			Status: http.StatusCreated,
+		}
+
+		jsonResponse, _ := json.Marshal(response)
+		
+		w.Write(jsonResponse)
+	}
 }
